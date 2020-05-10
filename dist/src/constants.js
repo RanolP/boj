abca9caee2a9a6abdc9c84c4a85c3c9b462b9142
RanolP <public.ranolp@gmail.com>
2020-05-10 21:31:24 +0900

    no more dist

--- dist/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const process_1 = require("process");
-exports.ROOT = path_1.resolve(process_1.cwd());
--- dist/src/util/date.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function formatDate(date) {
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-exports.formatDate = formatDate;
